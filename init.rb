daf39defa3db0e0ef3b0d2f00d0fac2065696dce
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-02-06 19:21:44 +0100

    use the same version number as on redmine.org
    use the same version number as on http://www.redmine.org/plugins/redmine_hotkeys_js

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_hotkeys_js do
   name 'Redmine Hotkeys Js plugin'
   author 'sasha-ch'
   description 'This is a plugin for Redmine'
-  version '2.6.0'
+  version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
