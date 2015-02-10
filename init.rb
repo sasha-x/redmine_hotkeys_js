44473caeb2beedc3e3d47d3009c1fb368769bab0
sasha-ch <o-n-i-x@yandex.ru>
2015-02-10 18:14:01 +0000

    * version fix

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_hotkeys_js do
   name 'Redmine Hotkeys Js plugin'
   author 'sasha-ch'
   description 'This is a plugin for Redmine which adds hotkeys for common actions.'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/sasha-ch/redmine_hotkeys_js'
   author_url 'https://github.com/sasha-ch'
 end
