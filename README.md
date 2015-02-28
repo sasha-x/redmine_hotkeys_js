4e8973dfac754bc0f2d250a1bddfb6fea40d2187
sasha-ch <o-n-i-x@yandex.ru>
2015-02-28 15:15:31 +0000

    + issue status change hotkeys

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # redmine_hotkeys_js
 ## Redmine Hotkeys Plugin
 
-Add some hotkeys for common actions
+Add some hotkeys for common actions: 
 
 * ctrl + enter : active form submit
+
+* project tabs switching
+
+* issue status changes
+
+
+### Project tabs switching
 
 * ctrl + alt + n : new issue
 
@@ -17,7 +24,35 @@ Add some hotkeys for common actions
 
 * ctrl + alt + g: gantt
 
-* ...
+### Issue status changes
+
+Based on default status list (script use ids, not names):
+New : 1
+In Progress : 2 (ctrl + shift + p)
+Resolved : 3 (ctrl + shift + r)
+Feedback : 4 (ctrl + shift + f)
+Closed : 5 (ctrl + shift + c)
+
+Where are some smart actions added for "Resolved" and "Feedback" shortcuts:
+
+* "Resolved" status set issue assignee to its author
+
+* "Feedback" return issue to previous assignee
+
+To disable it simply comment out lines 84:105 of js file.
+
+## Customize hotkeys
+
+In file: redmine_hotkeys_js/assets/javascripts/hotkeys.js
+you may edit vars: 
+
+* commonKeys
+
+* issueKeys
+
+* issueStatusMap
+
+and/or all other code you want :)
 
 ## Install 
 
@@ -29,4 +64,4 @@ Add some hotkeys for common actions
 
 ## Redmine compatibility
 
-2.x.x, 3.0.0
+2.5.x, 2.6.x, 3.0.0
--- assets/javascripts/hotkeys.js
+++ assets/javascripts/hotkeys.js
@@ -1,62 +1,112 @@
+/**
+ *  Redmine Hotkeys Plugin
+ *  Add some hotkeys for common actions: 
+ *  * ctrl + enter form submit
+ *  * project tabs switching
+ *  * issue status changes
+ */
 $(document).ready(function(){
-	//hotkeys
+	//hotkeys config
 	
-	//default base key combination for a bulk of hotkeys
+	//default base key combinations for a bulk of hotkeys
 	//may be ctrlKey, altKey, shiftKey
-	var defaultKeys = ['ctrlKey', 'altKey'];
+	var commonKeys = ['ctrlKey', 'altKey'];			//for tabs switching
+	var issueKeys = ['ctrlKey', 'shiftKey'];		//for issue status changes		
+	
+	//list of: 'status code' => 'status_id' 	//status name
+	//in your redmine
+	var issueStatusMap = {
+			//'n': 1,		//New
+			'p': 2,			//In Progress
+			'r': 3,			//Resolved
+			'f': 4,			//Feedback
+			'c': 5			//Closed
+		};
 	
 	$(document).bind('keydown', function(e){
 
 		//active form submit
 		//ctrl + enter
 		if(e.ctrlKey && e.keyCode == 13){
-			var form0 = $(':focus').closest('form');
-			window.onbeforeunload = undefined;
-		  	$(form0).submit();
+			submitActiveForm();
 		}
 	  
-		//new issue
-		//ctrl + alt + n
-		else if (ifKeysPressed(e, defaultKeys) && e.keyCode == 78){
-			$("li>a.new-issue:not(.selected)").hrefRedirect();
-		}
-
-	  	//new subtask
-		//ctrl + alt + m
-	  	else if(ifKeysPressed(e, defaultKeys) && e.keyCode == 77){
-	  		$('div.issue div#issue_tree>.contextual>a').hrefRedirect();
-		}
-		
-		//edit issue
-		//ctrl + alt + e
-		else if (ifKeysPressed(e, defaultKeys) && e.keyCode == 69){
-			if($("div#update")){
-				$("div#update").show();
-				$("textarea#issue_notes").focus();
+		//project tabs switching section
+		else if (ifKeysPressed(e, commonKeys)){
+			var fired = 1;
+			switch(String.fromCharCode(e.which).toLowerCase()){
+				//new issue
+				case 'n':
+					$("li>a.new-issue:not(.selected)").hrefRedirect();
+					break;
+				//new subtask
+				case 'm':
+					$('div.issue div#issue_tree>.contextual>a').hrefRedirect();
+					break;
+				//edit issue	
+				case 'e':
+					if($("div#update")){
+						$("div#update").show();
+						$("textarea#issue_notes").focus();
+					}
+					break;
+				//issues list	
+				case 'i':
+					$("li>a.issues").hrefRedirect();
+					break;
+				//wiki	
+				case 'w':
+					$("li>a.wiki").hrefRedirect();
+					break;
+				//gantt	
+				case 'g':
+					$("li>a.gantt").hrefRedirect();
+					break;
+				//for new hotkeys
+				default:
+					console.log(e);
+					fired = 0;
+					break;
 			}
+			if(fired)
+				e.preventDefault();
 		}
 		
-		//issues list
-		//ctrl + alt + i
-		else if (ifKeysPressed(e, defaultKeys) && e.keyCode == 73){
-			$("li>a.issues").hrefRedirect();
-		}
+		//issue status change section
 		
-		//wiki
-		//ctrl + alt + w
-		else if (ifKeysPressed(e, defaultKeys) && e.keyCode == 87){
-			$("li>a.wiki").hrefRedirect();
-		}
-		
-		//gantt
-		//ctrl + alt + g
-		else if (ifKeysPressed(e, defaultKeys) && e.keyCode == 71){
-			$("li>a.gantt").hrefRedirect();
-		}
-		
-		//for new hotkeys
-		else if(ifKeysPressed(e, defaultKeys)){
-		  	console.log(e);
+		//ctrl + shift + p
+		else if (ifKeysPressed(e, issueKeys)){
+			var key = String.fromCharCode(e.which).toLowerCase();
+			var status_id = issueStatusMap[key];
+			if(status_id){
+				$("select#issue_status_id").val(status_id);
+				
+				switch(key){
+					case 'r':
+						//"Resolved": pass issue to its author
+						var author_id = $('p.author > a.user').attr('href').split('/').pop();
+						$('select#issue_assigned_to_id').val(author_id);
+						break;
+					case 'f':
+						//"Feedback": pass issue to previous assignee
+						var assigned_to_id = $('td.assigned-to > a.user').attr('href').split('/').pop();
+						var prev_assignees = [];
+						$('div#history div.journal h4 a.user').each(function(){
+							var h_user_id = $(this).attr('href').split('/').pop();
+							if(h_user_id != assigned_to_id){
+								prev_assignees.push(h_user_id);
+							}
+						});
+						if(prev_assignees.length > 0){
+							prev_assignee = prev_assignees.pop();
+							$('select#issue_assigned_to_id').val(author_id);
+						}
+						break;
+				}
+				
+				submitActiveForm();
+				e.preventDefault();
+			}
 		}
 		
 		
@@ -66,7 +116,15 @@ $(document).ready(function(){
 
 });
 
-function ifKeysPressed(event, keys){
+function submitActiveForm()
+{
+	var form0 = $(':focus').closest('form');
+	window.onbeforeunload = undefined;
+	$(form0).submit();
+}
+
+function ifKeysPressed(event, keys)
+{
 	for(var i = 0; i < keys.length; i++){
 		if(event[keys[i]] != true)
 			return false;
@@ -74,9 +132,10 @@ function ifKeysPressed(event, keys){
 	return true;
 }
 
-;(function($) {
+;(function($){
 	//redirect page to href in current element
-	$.fn.hrefRedirect = function() {
+	$.fn.hrefRedirect = function()
+	{
 		var href = $(this).attr('href');
 		if(href > ''){
 			window.location.href = href;
