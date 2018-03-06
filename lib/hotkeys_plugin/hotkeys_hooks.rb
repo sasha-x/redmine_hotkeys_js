66ff3c6cb141ad7000499ec72bb8035862d2f2a2
Aleksandr <sasha-ch@users.noreply.github.com>
2018-03-06 16:50:34 +0300

    Update hotkeys_hooks.rb

--- lib/hotkeys_plugin/hotkeys_hooks.rb
+++ lib/hotkeys_plugin/hotkeys_hooks.rb
@@ -6,8 +6,6 @@ module HotkeysPlugin
         <<-TAGS
           #{stylesheet_link_tag 'hotkeys', :plugin => 'redmine_hotkeys_js'}
           #{javascript_include_tag 'hotkeys', :plugin => 'redmine_hotkeys_js'}
-          <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.1/css/select2.min.css" rel="stylesheet" />
-			<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.1/js/select2.min.js"></script>
         TAGS
       end
 
